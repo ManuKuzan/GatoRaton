db6b3479b5a3c6278167cc037ed25d9441d09532
ManuKuzan <136617152+ManuKuzan@users.noreply.github.com>
2023-07-03 17:25:56 -0600

    AvanceGatoManuel

---
+++ logica.js
@@ -0,0 +1,94 @@
+
+let matriz=[
+    ["a1","a2","a3"],
+    ["a4","a5","a6"],
+    ["a7","a8","a9"]
+]
+console.log(matriz[0][1]);
+console.log(matriz[1][2]);
+console.log(matriz[2][0]);
+console.log(matriz[0][0]);
+console.log(matriz[2][2]);
+console.log(matriz[1][1]);
+console.log(matriz[0][2]);
+console.log(matriz[2][1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("===================Practica==========================");
+
+// const matrisObjetos = [
+//   ["Manzana", "Plátano", "Naranja"],
+//   ["Perro", "Gato", "Pájaro"],
+//   ["Mesa", "Silla", "Lámpara"],
+// ];
+
+// console.log(matrisObjetos[1][1]);
+
+// // Resultado esperado//   Gato
+
+
+// console.log(matrisObjetos[2][2]);
+// // Resultado esperado//   Lámpara
+
+
+// console.log(matrisObjetos[0][0]);
+
+
+// console.log(matrisObjetos[2][1]);
+
+
+// console.log(matrisObjetos[0][2]);
+
+// let filaNaranja  /** tu codigo aqui */;
+// let columnaNaranja /** tu codigo aqui */;
+
+// //ejemplo;
+// let filaGato = 1;
+// let columnaGato = 1;
+
+// console.log(`La naranja esta en la posicion: fila ${filaNaranja} y la columna ${columnaNaranja}`);
+
+
+
+
+
+
+// let filaMesa  /** tu codigo aqui */;
+// let columnaMesa /** tu codigo aqui */;
+
+
+// console.log(`La mesa esta en la posicion: fila ${filaMesa} y la columna ${columnaMesa}`);
+
+
+
+
+// console.log(`4) Utiliza un ciclo for para imprimir los elementos de la fila numero #2  ;`);
+// // Resultado esperado//    'Mesa',
+// //                         'Silla'
+// //                         'Lámpara'
+
